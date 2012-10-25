facabd95bcd73788c29913ddf63b145381c0fac5
Lester Angulo <lester.dotcreek.com>
2012-10-25 08:45:09 -0600

    finish changes to master

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,8 +1,10 @@
 class Entity < ActiveRecord::Base
-	has_one :employee, :dependent => :destroy
+	
+  has_one :employee, :dependent => :destroy
 	has_many :telephones, :dependent => :destroy
   has_many :emails, :dependent => :destroy
   has_many :addresses, :dependent => :destroy
+  has_many :contacts, :dependent => :destroy
   has_one :customer
   has_one :vendor
 	accepts_nested_attributes_for :telephones, :allow_destroy => true, :reject_if => proc { |attributes| attributes["telephone"].blank? }
@@ -11,6 +13,6 @@ class Entity < ActiveRecord::Base
   accepts_nested_attributes_for :contacts, :allow_destroy => true   
   
   attr_accessible :entityid, :name, :surname, :typeid, :telephones_attributes, 
-                  :emails_attributes, :addresses_attributes, :contacts_attributes,
+                  :emails_attributes, :addresses_attributes, :contacts_attributes
 end
 
