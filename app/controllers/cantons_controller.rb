c766ca5e704026cea9f7665b6220bb171bf34116
lesterzone <lester.angulo@dotcreek.com>
2013-01-14 11:27:21 -0600

    DCERP-31 create/edit customer fixed

--- app/controllers/cantons_controller.rb
+++ app/controllers/cantons_controller.rb
@@ -1,30 +1,16 @@
 class CantonsController < ApplicationController
   
   after_filter :clean_cache, :only => [:new, :edit, :destroy]
-  # GET /cantons
-  # GET /cantons.json
+  respond_to :json, :html
+
   def index
-    @cantons = Canton.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @cantons }
-    end
+    respond_with @cantons = Canton.all
   end
 
-  # GET /cantons/1
-  # GET /cantons/1.json
   def show
-    @canton = Canton.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @canton }
-    end
+    respond_with @canton = Canton.find(params[:id])
   end
 
-  # GET /cantons/new
-  # GET /cantons/new.json
   def new
     @canton = Canton.new
     @province = Province.all
@@ -40,8 +26,6 @@ class CantonsController < ApplicationController
     @canton = Canton.find(params[:id])
   end
 
-  # POST /cantons
-  # POST /cantons.json
   def create
     @canton = Canton.new(params[:canton])
 
@@ -56,8 +40,6 @@ class CantonsController < ApplicationController
     end
   end
 
-  # PUT /cantons/1
-  # PUT /cantons/1.json
   def update
     @canton = Canton.find(params[:id])
 
@@ -72,8 +54,6 @@ class CantonsController < ApplicationController
     end
   end
 
-  # DELETE /cantons/1
-  # DELETE /cantons/1.json
   def destroy
     @canton = Canton.find(params[:id])
     @canton.destroy
@@ -84,6 +64,10 @@ class CantonsController < ApplicationController
     end
   end
 
+  def fetch
+    respond_with @canton = Canton.fetch
+  end
+  
   def clean_cache
     Canton.clean_cache
   end
--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -1,30 +1,16 @@
 class DistrictsController < ApplicationController
 
   after_filter :clean_cache, :only => [:new, :edit, :destroy]
-  # GET /districts
-  # GET /districts.json
+  respond_to :json, :html
+  
   def index
-    @districts = District.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @districts }
-    end
+    respond_with @districts = District.all
   end
 
-  # GET /districts/1
-  # GET /districts/1.json
   def show
-    @district = District.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @district }
-    end
+    respond_with @district = District.find(params[:id])
   end
 
-  # GET /districts/new
-  # GET /districts/new.json
   def new
     @district = District.new
     @cantons = Canton.all
@@ -36,15 +22,12 @@ class DistrictsController < ApplicationController
     end
   end
 
-  # GET /districts/1/edit
   def edit
     @district = District.find(params[:id])
     @cantons = Canton.all
     @provinces = Province.all
   end
 
-  # POST /districts
-  # POST /districts.json
   def create
     @district = District.new(params[:district])
 
@@ -85,6 +68,10 @@ class DistrictsController < ApplicationController
       format.html { redirect_to districts_url }
       format.json { head :no_content }
     end
+  end
+
+  def fetch
+    respond_with @district = District.fetch
   end
 
   def clean_cache
