4c5a34f7f2969e8109c0a8385f44b845ed195fd5
Kenneth Alfaro <kenneth.alfaro@dotcreek.com>
2015-09-03 16:34:21 -0600

    Fix problem in General Payroll  Changes to be committed: modified:   app/controllers/reports_controller.rb

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -18,6 +18,7 @@ class ReportsController < ApplicationController
     case params[:type].to_s
       
       when CONSTANTS[:REPORTS]['PAYMENT_PROOF']
+
         @employees = params[:employees].split(",")
         @payroll = Payroll.find(params[:payroll_id])
         @msg = params[:msg]
@@ -38,10 +39,9 @@ class ReportsController < ApplicationController
 
         @data = general_payroll_data(@payroll_ids, @employees)
 
-        puts "--------------------------"
-        puts @data
-        puts "--------------------------"
-
+        #puts "--------------------------"
+        #puts @data
+        #puts "--------------------------"
 
         if @format.to_s == "pdf"
 
@@ -154,79 +154,79 @@ class ReportsController < ApplicationController
 
         detail = {}
 
-      # G E T   E M P L O Y E   N A M E
-        e = Employee.find(employee_id)
-        name = {}
-        index = nil
-        name['Nombre Empleado'] = "#{e.entity.surname} #{e.entity.name}"
-        
-        if( employees_names.include? name )
-          index = employees_names.index(name)
-        else
-          detail['Nombre Empleado'] = "#{e.entity.surname} #{e.entity.name}"
-          employees_names << name
-        end
-        totals['Nombre Empleado'] = "Total"
-      # G E T   E M P L O Y E   N A M E
-        
-      # G E T   T O T A L   E A R N
-        total_earn = 0
-        PayrollHistory.where('payroll_log_id = ?', payroll_id).each do |p|
-          unless p.payroll_employees.where('employee_id = ?', employee_id).empty?
-            total_earn += p.total.to_f
-          end
-        end # End PayrollHistory
-        detail['Total Devengado'] = total_earn
-        if totals['Total Devengado'].blank?
-          totals['Total Devengado'] = total_earn
-        else
-          totals['Total Devengado'] += total_earn
-        end
-      # G E T   T O T A L   E A R N
-
-      # G E T   D E D U C T I O N S
-        # Set default values
-        list_deductions.each do |id|
-          d = Deduction.find(id)
-          list_desc_deductions["#{d.description}"] = "#{d.description}"
-          detail["#{d.description}"] = 0
-          if totals["#{d.description}"].blank?
-            totals["#{d.description}"] = 0
+        # G E T   E M P L O Y E   N A M E
+          e = Employee.find(employee_id)
+          name = {}
+          index = nil
+          name['Nombre Empleado'] = "#{e.entity.surname} #{e.entity.name}"
+          
+          if( employees_names.include? name )
+            index = employees_names.index(name)
           else
-            totals["#{d.description}"] += 0
+            detail['Nombre Empleado'] = "#{e.entity.surname} #{e.entity.name}"
+            employees_names << name
           end
-        end
+          totals['Nombre Empleado'] = "Total"
+        # G E T   E M P L O Y E   N A M E
+          
+        # G E T   T O T A L   E A R N
+          total_earn = 0
+          PayrollHistory.where('payroll_log_id = ?', payroll_id).each do |p|
+            unless p.payroll_employees.where('employee_id = ?', employee_id).empty?
+              total_earn += p.total.to_f
+            end
+          end # End PayrollHistory
+          detail['Total Devengado'] = total_earn
+          if totals['Total Devengado'].blank?
+            totals['Total Devengado'] = total_earn
+          else
+            totals['Total Devengado'] += total_earn
+          end
+        # G E T   T O T A L   E A R N
 
-        detail["Otras Deducciones"] = 0
-        if totals["Otras Deducciones"].blank?
-          totals["Otras Deducciones"] = 0
-        else
-          totals["Otras Deducciones"] = 0
-        end
-
-        total_deductions = 0
-        DeductionPayment.where('payroll_id = ?', payroll_id).each do |a|
-          if a.deduction_employee.employee_id.to_f == employee_id.to_f
-            if list_deductions.include?(a.deduction_employee.deduction.id)
-              detail["#{a.deduction_employee.deduction.description}"] += a.payment.to_f
-              totals["#{a.deduction_employee.deduction.description}"] += a.payment.to_f
-              total_deductions += a.payment.to_f
+        # G E T   D E D U C T I O N S
+          # Set default values
+          list_deductions.each do |id|
+            d = Deduction.find(id)
+            list_desc_deductions["#{d.description}"] = "#{d.description}"
+            detail["#{d.description}"] = 0
+            if totals["#{d.description}"].blank?
+              totals["#{d.description}"] = 0
             else
-              if list_deductions.count < 4
-                list_deductions.push a.deduction_employee.deduction.id
-                detail["#{a.deduction_employee.deduction.description}"] = a.payment.to_f
-                totals["#{a.deduction_employee.deduction.description}"] = a.payment.to_f
+              totals["#{d.description}"] += 0
+            end
+          end
+
+          detail["Otras Deducciones"] = 0
+          if totals["Otras Deducciones"].blank?
+            totals["Otras Deducciones"] = 0
+          else
+            totals["Otras Deducciones"] = 0
+          end
+
+          total_deductions = 0
+          DeductionPayment.where('payroll_id = ?', payroll_id).each do |a|
+            if a.deduction_employee.employee_id.to_f == employee_id.to_f
+              if list_deductions.include?(a.deduction_employee.deduction.id)
+                detail["#{a.deduction_employee.deduction.description}"] += a.payment.to_f
+                totals["#{a.deduction_employee.deduction.description}"] += a.payment.to_f
                 total_deductions += a.payment.to_f
               else
-                detail["Otras Deducciones"] += a.payment.to_f
-                totals["Otras Deducciones"] += a.payment.to_f
-                total_deductions += a.payment.to_f
-              end
-            end #End include
-          end
-        end # End WorkBenefitsPayment
-        list_desc_deductions["Otras Deducciones"] = "Otras Deducciones"
-      # G E T   D E D U C T I O N S
+                if list_deductions.count < 4
+                  list_deductions.push a.deduction_employee.deduction.id
+                  detail["#{a.deduction_employee.deduction.description}"] = a.payment.to_f
+                  totals["#{a.deduction_employee.deduction.description}"] = a.payment.to_f
+                  total_deductions += a.payment.to_f
+                else
+                  detail["Otras Deducciones"] += a.payment.to_f
+                  totals["Otras Deducciones"] += a.payment.to_f
+                  total_deductions += a.payment.to_f
+                end
+              end #End include
+            end
+          end # End WorkBenefitsPayment
+          list_desc_deductions["Otras Deducciones"] = "Otras Deducciones"
+        # G E T   D E D U C T I O N S
 
         detail["Total"] = (total_earn-total_deductions)
         if totals["Total"].blank?
@@ -238,11 +238,13 @@ class ReportsController < ApplicationController
         if(index.nil?)
           if detail['Total Devengado'] != 0
             list_payrolls << detail
+          else
+            employees_names.delete(name)
           end
         else
           list_payrolls[index]["Total Devengado"] += detail["Total Devengado"]
           list_payrolls[index]["Total"] += detail["Total"]
-          
+
           list_desc_deductions.each do |i, value|
             list_payrolls[index]["#{value}"] += detail["#{value}"]
           end
