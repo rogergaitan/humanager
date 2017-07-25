abae0ea5f7ce8e4ea1a808d536b18aae6b3e9e33
Roger Gaitan <roger.gaitan@dotcreek.com>
2017-07-25 16:49:23 -0600

    add currencies controller

---
+++ app/controllers/currencies_controller.rb
@@ -0,0 +1,25 @@
+class CurrenciesController < ApplicationController
+  before_filter :set_currency, only: [:edit, :update]
+  
+  def index
+    @currencies = Currency.all
+  end
+    
+  def edit
+  end
+  
+  def update
+    if @currency.update_attributes params[:currency]
+      redirect_to currencies_path, notice: "Moneda actualizada correctamente"
+    else
+      render :edit
+    end
+  end
+  
+  private
+  
+    def set_currency
+      @currency = Currency.find params[:id]    
+    end
+  
+end
