baa0ba91caa2d01e16ba383aadb7b8bb2ab0c66f
Roger Gaitan <roger.gaitan@dotcreek.com>
2017-07-26 11:16:35 -0600

    update currency model

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,5 +1,5 @@
 class Currency < ActiveRecord::Base
   attr_accessible :name, :symbol
   
-  validate :name, :symbol, :currency_type, presence: true
+  validates :name, :symbol, :currency_type, presence: true
 end
