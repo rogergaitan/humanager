a7006317e1326769a5280aabf448a6901b4735e5
Roger Gaitan <roger.gaitan@dotcreek.com>
2017-08-08 10:14:13 -0600

    update payment types controller to search by company id

--- app/models/payment_type.rb
+++ app/models/payment_type.rb
@@ -2,5 +2,8 @@ class PaymentType < ActiveRecord::Base
   attr_accessible :description, :factor, :name, :contract_code, :payment_type, :state
 
   scope :all_payment_types, where('state = ?', CONSTANTS[:PAYROLLS_STATES]['ACTIVE'])
-
+  
+  def self.find_by_company_id(company_id)
+    where(company_id: company_id)
+  end
 end
