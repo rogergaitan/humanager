472eeb294d7d240346c55031fec829d7758f7b78
Roger Gaitan <roger.gaitan@dotcreek.com>
2017-08-08 15:38:22 -0600

    update previous migration

--- db/migrate/20170808213053_add_company_ref_to_payment_type.rb
+++ db/migrate/20170808213053_add_company_ref_to_payment_type.rb
@@ -2,6 +2,4 @@ class AddCompanyRefToPaymentType < ActiveRecord::Migration
   def change
     add_column :payment_types, :company_id, :integer
   end
-  
-  add_index :payment_types, :company_id
 end
