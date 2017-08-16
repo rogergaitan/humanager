fdfe928924f5cc547105164f6f59d3e9cb839455
Roger Gaitan <roger.gaitan@dotcreek.com>
2017-08-16 14:52:03 -0600

    update task search method

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,10 +6,10 @@ class Task < ActiveRecord::Base
 
  	def self.search(search_activity, search_code, search_desc, currency, page)
     query = Task.includes(:currency)
-    query = Task.where("nactivity LIKE ?", "#{search_activity}%") unless search_activity.empty?
-    query = Task.where("itask LIKE ?", "#{search_code}%") unless search_code.empty?
-    query = Task.where("ntask LIKE ?", "#{search_desc}%") unless search_desc.empty?
-    query  = Task.where(currency_id: currency) unless currency.empty?
+    query = query.where("nactivity LIKE ?", "#{search_activity}%") unless search_activity.empty?
+    query = query.where("itask LIKE ?", "#{search_code}%") unless search_code.empty?
+    query = query.where("ntask LIKE ?", "#{search_desc}%") unless search_desc.empty?
+    query  = query.where(currency_id: currency) unless currency.empty?
       
     query.paginate(:page => page, :per_page => 10)
 	end
