a69270623f0136e4ac77056051d5fe569b10f2eb
Roger Gaitan <roger.gaitan@dotcreek.com>
2017-08-28 15:37:55 -0600

    update ir table model

--- app/models/ir_table.rb
+++ app/models/ir_table.rb
@@ -1,3 +1,10 @@
 class IrTable < ActiveRecord::Base
-  attr_accessible :end_date, :name, :start_date
+  attr_accessible :end_date, :name, :start_date, :ir_table_values_attributes
+  
+  has_many :ir_table_values
+  
+  accepts_nested_attributes_for :ir_table_values
+  
+  validates :name, :start_date, :end_date, presence: true
+  
 end
