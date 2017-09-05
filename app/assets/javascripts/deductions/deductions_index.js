3f2dadd1b83944f703764858694ecbbf7886372c
Roger Gaitan <roger.gaitan@dotcreek.com>
2017-09-05 14:16:46 -0600

    add clean search button on deductions index

---
+++ app/assets/javascripts/deductions/deductions_index.js
@@ -0,0 +1,28 @@
+$(".pagination a[href]").attr("data-remote", true);
+
+$("#clean_button").on("click", function () {
+  cleanSearch();
+});
+
+$("#deduction_type, #calculation_type, #state").on("change", function () {
+  search();
+}); 
+
+function search () {
+  $.ajax({
+    url: "deductions/search",
+    dataType: "script",
+    data: {
+      deduction_type: $("#deduction_type").val(),
+      calculation_type: $("#calculation_type").val(),
+      state: $("#state").val()
+    }
+  });
+}
+
+function cleanSearch() {
+  $.ajax({
+    url: "deductions/",
+    dataType: "script"
+  });
+}
