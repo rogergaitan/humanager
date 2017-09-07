17c366fdc8c9f1cdf6bf700655dbc1f73be6849d
Roger Gaitan <roger.gaitan@dotcreek.com>
2017-09-07 10:16:09 -0600

    update supports table migration

--- db/migrate/20170907154851_create_supports.rb
+++ db/migrate/20170907154851_create_supports.rb
@@ -1,7 +1,7 @@
 class CreateSupports < ActiveRecord::Migration
   def change
     create_table :supports do |t|
-      t.string :itdsop
+      t.integer :itdsop
       t.string :ntdsop
       t.string :smask
     end
