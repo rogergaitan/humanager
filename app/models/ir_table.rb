d557b180c941edca9361cacc1e70ddc3af10ea00
Roger Gaitan <roger.gaitan@dotcreek.com>
2017-09-11 13:05:28 -0600

    update ir table validations

--- app/models/ir_table.rb
+++ app/models/ir_table.rb
@@ -1,7 +1,7 @@
 class IrTable < ActiveRecord::Base
   attr_accessible :end_date, :name, :start_date, :ir_table_values_attributes
   
-  has_many :ir_table_values, dependent: :destroy
+  has_many :ir_table_values, dependent: :destroy, validate: true
   
   accepts_nested_attributes_for :ir_table_values, allow_destroy: true
   
