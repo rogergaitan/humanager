ce3451682680a17e165d919cccf11b1d6efb7f43
Roger Gaitan <roger.gaitan@dotcreek.com>
2017-09-12 17:05:34 -0600

    update ir table values backend validation

--- app/models/ir_table.rb
+++ app/models/ir_table.rb
@@ -1,7 +1,7 @@
 class IrTable < ActiveRecord::Base
   attr_accessible :end_date, :name, :start_date, :ir_table_values_attributes
   
-  has_many :ir_table_values, dependent: :destroy, validate: true
+  has_many :ir_table_values, dependent: :destroy, inverse_of: :ir_table
   
   accepts_nested_attributes_for :ir_table_values, allow_destroy: true
   
@@ -10,11 +10,14 @@ class IrTable < ActiveRecord::Base
   validate :validate_ir_table_values
   
   def validate_ir_table_values
-    self.ir_table_values.each_with_index do |ir_table, index|
+    self.ir_table_values.each_with_index do |ir_table_value, index|
+      
       unless index == 0
-         if ir_table.from <= self.ir_table_values[index -1].until
-           errors.add(:base, "Valor del campo desde debe ser al menos un numero mayor que el campo hasta del estrato anterior")  
+        unless ir_table_value.from.nil? || self.ir_table_values[index -1].until.nil?
+          if ir_table_value.from <= self.ir_table_values[index -1].until
+           errors.add :base, "Valor del campo de debe ser al menos un numero mayor que el campo hasta del estrato anterior"
          end
+        end
       end
     end
   end
