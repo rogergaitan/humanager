2b8c05dc67dde69b9d3adc41f926391b2d407d70
Roger Gaitan <roger.gaitan@dotcreek.com>
2017-09-26 15:35:01 -0600

    add calculation type column

---
+++ db/migrate/20170926212656_add_work_benefit_type_to_work_benefit.rb
@@ -0,0 +1,12 @@
+class AddWorkBenefitTypeToWorkBenefit < ActiveRecord::Migration
+  def up
+    change_table :work_benefits do |t|
+      t.column :calculation_type, :enum, limit: [:percentage, :fixed]
+    end
+  end
+  
+  def down
+    remove_column :work_benefits, :calculation_type
+  end
+  
+end
