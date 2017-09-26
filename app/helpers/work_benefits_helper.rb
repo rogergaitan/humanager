a71dae4c22faf201b25f5f69b734ed749c418133
Roger Gaitan <roger.gaitan@dotcreek.com>
2017-09-26 16:56:48 -0600

    update work benefit index view

--- app/helpers/work_benefits_helper.rb
+++ app/helpers/work_benefits_helper.rb
@@ -1,2 +1,20 @@
 module WorkBenefitsHelper
+  def state_helper(state)
+    if state == :active
+      "Si"
+    else
+      "No"
+    end
+  end
+  
+  def work_benefits_value_helper(work_benefit)
+    if work_benefit.individual
+      "Individual"
+    elsif work_benefit.calculation_type == :percentage
+      "#{work_benefit.deduction_value}%"
+    elsif work_benefit.calculation_type == :fixed
+      "#{work_benefit.deduction_currency.try :symbol}#{work_benefit.deduction_value}"
+    end 
+  end
+  
 end
