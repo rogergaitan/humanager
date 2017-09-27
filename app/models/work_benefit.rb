b6f75d3dbe18b28a4a8830599ea36c3f9ae0dc29
Roger Gaitan <roger.gaitan@dotcreek.com>
2017-09-27 10:09:42 -0600

    add search method

--- app/models/work_benefit.rb
+++ app/models/work_benefit.rb
@@ -1,7 +1,7 @@
 class WorkBenefit < ActiveRecord::Base
   attr_accessible :credit_account, :debit_account, :description, :percentage, :employee_ids,
     :payroll_type_ids, :is_beneficiary, :beneficiary_id, :costs_center_id, :company_id, :name, :state, 
-    :calculation_type, :work_benefits_value, :work_benefits_type
+    :calculation_type, :work_benefits_value, :work_benefits_type, :currency_id
 
   belongs_to :company
   has_many :companies
@@ -15,10 +15,21 @@ class WorkBenefit < ActiveRecord::Base
   belongs_to :costs_center , class_name: 'CostsCenter', foreign_key: "costs_center_id"
   belongs_to :debit, class_name: 'LedgerAccount', foreign_key: "debit_account"
   belongs_to :credit, class_name: 'LedgerAccount', foreign_key: "credit_account"
+  belongs_to :currency
 
   def self.search_cost_center(search_cost_center_name, company_id, page, per_page = nil)
     @cost_center = CostsCenter.where("costs_centers.name_cc like '%#{search_cost_center_name}%' and company_id = '#{company_id}'")
     .paginate(:page => page, :per_page => 5)
+  end
+  
+  def self.search(work_benefits_type, calculation_type, state, company, page)
+    query = WorkBenefit.includes :currency
+    query = query.where company_id: company
+    query = query.where work_benefits_type: work_benefits_type unless work_benefits_type.empty?
+    query = query.where calculation_type: calculation_type unless calculation_type.empty?
+    query = query.where state: state unless  state.empty?
+    
+    query.paginate page: page, per_page: 15
   end
 
 end
