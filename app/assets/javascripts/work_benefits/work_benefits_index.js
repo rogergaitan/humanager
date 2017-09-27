38a1b2d60892486ddf5366d66be554851a69cd83
Roger Gaitan <roger.gaitan@dotcreek.com>
2017-09-27 10:10:06 -0600

    add ajax search

---
+++ app/assets/javascripts/work_benefits/work_benefits_index.js
@@ -0,0 +1,29 @@
+$(".pagination a[href]").attr("data-remote", true);
+
+$("#clean_button").on("click", function () {
+  cleanSearch();
+});
+
+$("#work_benefits_type, #calculation_type, #state").on("change", function () {
+  search();
+}); 
+
+function search () {
+  $.ajax({
+    url: "/work_benefits/search",
+    dataType: "script",
+    data: {
+      work_benefits_type: $("#work_benefits_type").val(),
+      calculation_type: $("#calculation_type").val(),
+      state: $("#state").val()
+    }
+  });
+}
+
+function cleanSearch() {
+  $.ajax({
+    url: "work_benefits/",
+    dataType: "script"
+  });
+}
+ 
