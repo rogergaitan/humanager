a0eb6807886cfbc347ab8bd83418b00081923368
Roger Gaitan <roger.gaitan@dotcreek.com>
2017-09-28 13:34:05 -0600

    add calculation property

--- app/models/employee_benefit.rb
+++ app/models/employee_benefit.rb
@@ -1,5 +1,5 @@
 class EmployeeBenefit < ActiveRecord::Base
-  attr_accessible :employee_id, :work_benefit_id, :completed
+  attr_accessible :employee_id, :work_benefit_id, :completed, :calculation
   belongs_to :work_benefit
   belongs_to :employee
   has_many :work_benefits_payments, foreign_key: 'employee_benefits_id'
