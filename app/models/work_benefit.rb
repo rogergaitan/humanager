e51b7a7304e0b7afb804dda923553ae41f2daa32
Roger Gaitan <roger.gaitan@dotcreek.com>
2017-10-04 08:51:10 -0600

    update work benefit payroll association

--- app/models/work_benefit.rb
+++ app/models/work_benefit.rb
@@ -2,21 +2,19 @@ class WorkBenefit < ActiveRecord::Base
   attr_accessible :credit_account, :debit_account, :name, :percentage, :employee_ids,
     :payroll_type_ids, :is_beneficiary, :beneficiary_id, :costs_center_id, :company_id, :name, :state, 
     :calculation_type, :work_benefits_value, :work_benefits_type, :currency_id, :active, :provisioning, 
-    :individual, :currency, :employee_benefits_attributes, :creditor_id, :pay_to_employee
+    :individual, :currency, :employee_benefits_attributes, :creditor_id, :pay_to_employee, :payroll_id
 
   attr_accessor :active
   
   belongs_to :company
   has_many :companies
-  
   has_many :payroll_type_benefits, :dependent => :destroy
   has_many :payroll_type, :through => :payroll_type_benefits, validate: false
   has_many :employee_benefits, :dependent => :destroy
   has_many :employees, :through => :employee_benefits, validate: false
   has_many :work_benefits_payments
-  has_many :work_benefits_payrolls, :dependent => :destroy
-  has_many :payrolls, :through => :work_benefits_payrolls
   
+  belongs_to :payroll
   belongs_to :costs_center , class_name: 'CostsCenter', foreign_key: "costs_center_id"
   belongs_to :debit, class_name: 'LedgerAccount', foreign_key: "debit_account"
   belongs_to :credit, class_name: 'LedgerAccount', foreign_key: "credit_account"
