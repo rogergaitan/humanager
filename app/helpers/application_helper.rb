5caa2b44821d52a14ff91c0d0920e9ab910abeb5
Roger Gaitan <roger.gaitan@dotcreek.com>
2017-10-10 08:46:29 -0600

    update value helper to receive currency relation method parameter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -75,13 +75,13 @@ module ApplicationHelper
   
   #used by models with calculation and individual attributes
   #to show individual, percent or currency value
-  def value_helper(model, value_method)
+  def value_helper(model, value_method, currency)
     if model.individual
       "Individual"
     elsif model.calculation_type == :percentage
       "#{model.send(value_method)}%"
     elsif model.calculation_type == :fixed
-      "#{model.currency.try :symbol}#{model.send(value_method)}"
+      "#{model.send(currency).try :symbol}#{model.send(value_method)}"
     end 
   end
 
