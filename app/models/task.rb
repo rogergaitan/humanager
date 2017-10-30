1968bcc0e21aa0fd486e52d2d455062398621fb2
Roger Gaitan <roger.gaitan@dotcreek.com>
2017-10-30 14:31:22 -0600

    fix indentation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,10 @@
 class Task < ActiveRecord::Base
 
-	has_many :payroll_logs
+  has_many :payroll_logs
   belongs_to :currency
- 	attr_accessible  :iactivity, :itask, :ntask, :nunidad, :currency_id, :cost, :nactivity
+  attr_accessible :iactivity, :itask, :ntask, :nunidad, :currency_id, :cost, :nactivity
 
- 	def self.search(search_activity, search_code, search_desc, currency, page)
+  def self.search(search_activity, search_code, search_desc, currency, page)
     query = Task.includes(:currency)
     query = query.where("nactivity LIKE ?", "#{search_activity}%") unless search_activity.empty?
     query = query.where("itask LIKE ?", "#{search_code}%") unless search_code.empty?
@@ -12,5 +12,5 @@ class Task < ActiveRecord::Base
     query  = query.where(currency_id: currency) unless currency.empty?
       
     query.paginate(:page => page, :per_page => 10)
-	end
+  end
 end
