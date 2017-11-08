3bcade3103a3ad88c6e11b6971994d2566467437
Roger Gaitan <roger.gaitan@dotcreek.com>
2017-11-08 15:39:52 -0600

    add method to find value if individual

--- app/models/other_payment_payment.rb
+++ app/models/other_payment_payment.rb
@@ -3,4 +3,13 @@ class OtherPaymentPayment < ActiveRecord::Base
   belongs_to :payroll
   belongs_to :currency
   attr_accessible :payment, :payment_date, :other_payment_employee_id, :payroll_id
+  
+  def other_payment_value
+    if other_payment_employee.other_payment.individual?
+      other_payment_employee.calculation
+    else
+      other_payment_employee.other_payment.amount
+    end
+  end
+  
 end
