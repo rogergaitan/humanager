a505c471bd2e01c3443ee2617e88a2b89eaaf9fa
Roger Gaitan <ragp1985@gmail.com>
2017-11-30 15:38:38 +0000

    Merged in feature/password_update (pull request #19)
    update password change form
    
    Approved-by: Kenneth Alfaro <kenneth.89.an@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,46 +22,22 @@ class User < ActiveRecord::Base
   has_many :permissions_user, :dependent => :destroy
   belongs_to :company
   has_many :companies
-  # after_create :create_permissions
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :encrypted_password, :password_confirmation, :remember_me, 
     :username, :name, :company_id
-  # attr_accessible :title, :body
-
-  validates :name,
-			:presence => true
   
-  validates :username,
-  				:presence => true,
-  				:length => { :within => 4..10 }
+  validates :name, :presence => true
+  
+  validates :username, :presence => true, :length => { :within => 4..10 }
 
-  validates :email,
-  			:presence => true,
-  			:uniqueness => true
-
-  after_create :send_welcome_email
-
-  def send_welcome_email
-    
-    puts "EMAIL CREATE USER"
-
-    # #email_object: hash with the email queue attributes
-    # #CONFIG is a hash constant that takes values from a configuration file (/config/application.yml)
-    # email_object = { user_id: id, to: email,
-    #   subject: CONFIG[:email_templates]['welcome']['subject'],
-    #   from: 'humanager.mailer.test@gmail.com' }
-    # #template_data: Hash for passing data to the email template
-    # template_data = { first_name: first_name, email: email }
-    # EmailQueue.save_email(email_object, CONFIG[:email_templates]['welcome']['template'], template_data)
-  end
-
+  validates :email, :presence => true, :uniqueness => true 
+  
   def self.search_users(username, name, actualuser, page, per_page)
     query = User.includes :permissions_user
     query = query.where("username LIKE ?", "%#{username}%") unless username.empty?
@@ -81,16 +57,16 @@ class User < ActiveRecord::Base
       transaction do
         data.each do |c|
           PermissionsUser.find_by_user_id_and_permissions_subcategory_id(user_id, c[1]['id_subcategory'])
-                          .update_attributes(
-                              :p_create => c[1]['p_create'],
-                              :p_view => c[1]['p_view'],
-                              :p_modify => c[1]['p_modify'],
-                              :p_delete => c[1]['p_delete'],
-                              :p_close => c[1]['p_close'],
-                              :p_accounts => c[1]['p_accounts'],
-                              :p_pdf => c[1]['p_pdf'],
-                              :p_exel => c[1]['p_exel']
-                          )
+            .update_attributes(
+              :p_create => c[1]['p_create'],
+              :p_view => c[1]['p_view'],
+              :p_modify => c[1]['p_modify'],
+              :p_delete => c[1]['p_delete'],
+              :p_close => c[1]['p_close'],
+              :p_accounts => c[1]['p_accounts'],
+              :p_pdf => c[1]['p_pdf'],
+              :p_exel => c[1]['p_exel']
+            )
           end # End
         end # End Transaction
     rescue Exception => exc
@@ -104,10 +80,4 @@ class User < ActiveRecord::Base
     @permissions = PermissionsUser.where('user_id=?', id)
   end
 
-  # def create_permissions
-  #   # self same to this
-  #   logger.debug 'create_permissions after_create'
-  #   logger.debug self.to_yaml
-  # end
-
-end
+end 
